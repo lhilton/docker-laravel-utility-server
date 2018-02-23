ac60cf02cd084e440091677a6eedc878a9ce0b7a
Sean Pearce <smpita@usa.net>
2018-02-22 19:04:46 -0800

    Call .env handler

--- root/scripts/update-app-from-git.sh
+++ root/scripts/update-app-from-git.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
+UPDATE_WWW_ENV=/root/scripts/update-www-env.sh
+
 cd /www
 git reset --hard HEAD
 git pull
 
-if [[ ! -e /www/.env ]]; then
-    ln -s $APP_ENV_FILE .env
-fi
+$UPDATE_WWW_ENV
 
 composer install
 php artisan migrate --force
